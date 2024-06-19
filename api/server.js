89f0f069f6ebd89a6c4cf7ccf08394e55bb44931
daw2-henares22 <91276223+daw2-henares22@users.noreply.github.com>
2024-06-19 16:26:41 +0200

    Add files via upload

--- api/server.js
+++ api/server.js
@@ -1,24 +1,24 @@
 // See https://github.com/typicode/json-server#module
-const jsonServer = require('json-server')
+import { create, router as _router, defaults, rewriter } from 'json-server'
 
-const server = jsonServer.create()
+const server = create()
 
 // Uncomment to allow write operations
-const fs = require('fs')
-const path = require('path')
-const filePath = path.join('tickets.json')
-const data = fs.readFileSync(filePath, "utf-8");
+import { readFileSync } from 'fs'
+import { join } from 'path'
+const filePath = join('tickets.json')
+const data = readFileSync(filePath, "utf-8");
 const tickets = JSON.parse(data);
-const router = jsonServer.router(tickets)
+const router = _router(tickets)
 
 // Comment out to allow write operations
 // const router = jsonServer.router('tickets.json')
 
-const middlewares = jsonServer.defaults()
+const middlewares = defaults()
 
 server.use(middlewares)
 // Add this before server.use(router)
-server.use(jsonServer.rewriter({
+server.use(rewriter({
     '/api/*': '/$1',
     '/blog/:resource/:id/show': '/:resource/:id'
 }))
@@ -28,4 +28,4 @@ server.listen(3000, () => {
 })
 
 // Export the Server API
-module.exports = server
+export default server
